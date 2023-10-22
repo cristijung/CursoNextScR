ab49507ca656e2ba23dad474ad067d9b5d969c9e
Cristina Jung <cristijung@gmail.com>
2023-10-22 16:21:48 -0300

    head

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
-import Link from 'next/link'
 
 export default function Home() {
   return (
   <>
   <section className="altura">
+  
     <h2>Welcome</h2>
   </section> 
   </>
