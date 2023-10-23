ea41989e2c1ecfe37f4b1c25fa6b9f12913109ac
Cristina Jung <cristijung@gmail.com>
2023-10-23 14:08:50 -0300

    apanhando para o css, como sempre

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,14 +1,37 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./server.module.scss";
 
-type Country = {
+export type Country = {
   name: {
     common: string;
-  }
-}
+  };
 
+  translations: {
+    por: {
+      common: string;
+    };
+  };
+
+  flags: {
+    svg: string;
+    alt: string;
+  };
+
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  languages?: {
+    [key: string]: string;
+  };
+
+  borders?: string[];
+  cca3: string;
+};
 async function getCountries(): Promise<Country[]> {
   const response = await fetch("https://restcountries.com/v3.1/all");
-  return response.json();  
+  return response.json();
 }
 
 //isso funciona no Next pq está rodando no servidor e não no browser
@@ -16,19 +39,27 @@ async function getCountries(): Promise<Country[]> {
 export default async function Server() {
   const countries = await getCountries();
 
-//console.log(countries);  //este log não aparece no browser, só no terminal da aplicação pq é um server component
+  //console.log(countries);  //este log não aparece no browser, só no terminal da aplicação pq é um server component
   return (
     <>
-      <section className="altura">
-        <h2>Server Component</h2>
-        <hr/>
-        <div className="conteudo">
+      <section className={styles.altura}>
+      <h2>Server Component</h2>
+      <hr />      
+        <div>
           {countries.map((country) => (
-            <h3 key={country.name.common}>{country.name.common}</h3>
+            <div key={country.name.common}>
+              <h3>{country.name.common}</h3>              
+              <div>
+              <Image
+                src={country.flags.svg}
+                alt={country.flags.alt}
+                fill                
+              />
+              </div>              
+            </div>
           ))}
-
-        </div>
-      </section>
+        </div>      
+    </section>
     </>
   );
 }
