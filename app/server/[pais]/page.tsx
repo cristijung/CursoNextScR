b63eeeca2520ff0c9bbfda72e9c519be43232bb1
Cristina Jung <cristijung@gmail.com>
2023-10-25 10:57:37 -0300

    server component pronto

--- app/server/[pais]/page.tsx
+++ app/server/[pais]/page.tsx
@@ -1,6 +1,7 @@
 import type { Country } from "@/app/server/page";
 import styles from "../server.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 async function getCountryByName(name: string): Promise<Country> {
   const response = await fetch(
@@ -15,6 +16,8 @@ export default async function CountryPage({
   params: { pais: string };
 }) {
   const country = await getCountryByName(pais);
+  const formato = Intl.NumberFormat("en", { notation: "compact" }); //internacionalização para formatar o número da pop
+
   return (
     <>
       <div className="altura">
@@ -23,20 +26,34 @@ export default async function CountryPage({
           <div className={styles.titulo}>{country.translations.por.common}</div>
           <main className={styles.box}>
             <section>
-                <h3>🏙️ Capital: {country.capital}</h3>
-                <h3>🗺️ Continente: {country.region} -  {country.subregion} </h3>
-                <h3>👨‍👩‍👧‍👦 População: {country.population} </h3>
+              <h3>🏙️ Capital: {country.capital}</h3>
+              <h3>
+                🗺️ Continente: {country.region} - {country.subregion}{" "}
+              </h3>
+              <h3>👨‍👩‍👧‍👦 População: {formato.format(country.population)} </h3>
               {country.languages && (
-                <h3>🗣️ Idiomas: 
+                <h3>
+                  🗣️ Idiomas: &nbsp;
                   {Object.values(country.languages).map((language) => (
-                    <span key={language}>{language}</span>
+                    <span key={language} className={styles.lang}>
+                      {language}
+                    </span>
                   ))}
                 </h3>
               )}
             </section>
+            <div className={styles.contentor}>
+              <Image
+                src={country.flags.svg}
+                alt={country.flags.alt}
+                width={400}
+                height={200}
+                className={styles.bandeira}
+              />
+            </div>
           </main>
-          <p style={{ width: '75%', margin: 'auto', marginTop: '10px' }}>
-            <Link href="/" className={styles.menu}>
+          <p style={{ width: "75%", margin: "auto", marginTop: "10px" }}>
+            <Link href="/server" className={styles.menu}>
               Voltar
             </Link>
           </p>
